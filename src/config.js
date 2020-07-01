9e28735a55e7b657c01862a8768c47b7553339c8
Cecilia <cecilliamarcellesi@gmail.com>
2020-07-01 14:20:37 +0100

    Setting up React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-west-2",
+    BUCKET: "ceci-notes-app",
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://54i2orbedc.execute-api.us-east-2.amazonaws.com/prod/",
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_QXiBnGDax",
+    APP_CLIENT_ID: "7gfhck9rhdrip6q1g1lprpj2ii",
+    IDENTITY_POOL_ID: "us-east-2:87961960-e14e-4876-a723-c6cef1bd184a",
+  },
+};
